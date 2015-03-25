ad4af9ee06e0ec1c5a0fdfa6a6401dba562d40c9
Jessica Hunt <huntres@me.com>
2015-03-24 18:15:03 -0700

    Added pub files

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top -110)
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+$(function() {
+    $('body').on('click', '.page-scroll-about a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$(function() {
+    $('body').on('click', '.page-scroll-contact  a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$("a").addClass("hover");
+
+$('.nav li a').addClass('big');
+
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+$('body').scrollspy({ target: '.navbar-default' })
